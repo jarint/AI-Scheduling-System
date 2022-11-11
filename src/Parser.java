96a7934f925e8f885cc13a65b0f67e07ba3a980e
jarint <56743248+jarint@users.noreply.github.com>
2022-11-11 01:10:31 -0700

    parser edit

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,7 @@
-package src;/*
- *  The src.Parser class takes input from a text file, parses that input, and put it into
- *  a data structure usable by our scheduler class.
+package src;
+/**
+ * The src.Parser class takes input from a text file, parses that input, and put it into
+ * a data structure usable by our scheduler class.
  */
 
 public class Parser {
