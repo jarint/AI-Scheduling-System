ac5393dec2a1f0320c9df7241ba6b21aa9f598a2
RaineLegary <rainethcubed@gmail.com>
2022-11-15 18:54:55 -0700

    deleted unnecessary files

--- src/Constraint.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-/**
- * Abstract class that will be the parent of all constraint classes, which will
- * be implemented as individual classes.
- */
-public abstract class Constraint {
-
-}
--- src/Practice.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-/**
- * src.Practice class stores practice schedule objects and their related attributes/functions
- */
-
-public class Practice extends ScheduleObject{
-
-}
