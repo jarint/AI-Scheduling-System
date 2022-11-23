2f69da682fc7a8daabc4e3bf4f77ec07dd6154ba
GabrahamLincoln <gabrielcameron99@gmail.com>
2022-11-23 11:57:44 -0700

    Fixed import issues

--- ScheduleObjects/Slot.py
+++ ScheduleObjects/Slot.py
@@ -4,7 +4,7 @@ corresponding to games and practices, which will both have attributes of this cl
 
 '''
 
-from ScheduleObjects.Activity import Activity
+from Activity import Activity
 
 class Slot:
     activities = []
