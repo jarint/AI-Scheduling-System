3ff2b51ae6bc972ae5746eef79813218c051db4b
GabrahamLincoln <gabrielcameron99@gmail.com>
2022-12-03 16:35:05 -0700

    Update Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -5,15 +5,13 @@ schedule at the end of the evaluation process.
 '''
 
 from ScheduleObjects.Schedule import Schedule
-from Search.Environment import Environment
 from Parser import Parser
 
 
 class Scheduler:
 
     def __init__(self) -> None:
-        self.env: Environment = Environment()
-        self.parser: Parser = Parser(self.env)
+        self.parser: Parser = Parser()
 
     
     # start function internal methods start the search process
