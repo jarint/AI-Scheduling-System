2ecddead27e1a27f917653e599d0ea56f29e63f5
GabrahamLincoln <gabrielcameron99@gmail.com>
2022-12-03 14:00:15 -0700

    Added div, opt, erw, and fleaf to Tree class

--- Search/SearchModel.py
+++ Search/SearchModel.py
@@ -4,46 +4,50 @@ from Constraints.HardConstraints import HardConstraints
 from Constraints.SoftConstraints import SoftConstraints
 from Search.Environment import Environment
 
-def div(schedule: Schedule):
-    """
-    Input: A schedule to expand.
-    Output: A list of all possible schedules produced by assigning a single game or practice to any available slot, where that assignment does not violate hard constraints.
-    Note that we will never generate a new schedule (we only produce copies).
-        Therefore, the only schedule 'initialized' is the very first empty schedule.
-        In this way, the integrity of the 'remaining_games' and 'remaining_practices' variables is preserved.
-        Game and practice are removed from their respective 'remaining' lists in a schedule upon being assigned to the schedule.
-    """
-    assignments = [] # list of tuples, where each represents an assignment
+class SearchModel:
 
-    # Generating possible assignments for games
-    for game_id in schedule.remaining_games:
-        for slot_id in Environment.GAME_SLOT_IDS:
-            assignment = tuple(game_id, slot_id)
-            if HardConstraints.check_constraints(schedule, assignment):
-                assignments.append(assignment)
+    @staticmethod
+    def div(schedule: Schedule):
+        """
+        Input: A schedule to expand.
+        Output: A list of all possible schedules produced by assigning a single game or practice to any available slot, where that assignment does not violate hard constraints.
+        Note that we will never generate a new schedule (we only produce copies).
+            Therefore, the only schedule 'initialized' is the very first empty schedule.
+            In this way, the integrity of the 'remaining_games' and 'remaining_practices' variables is preserved.
+            Game and practice are removed from their respective 'remaining' lists in a schedule upon being assigned to the schedule.
+        """
+        assignments = [] # list of tuples, where each represents an assignment
 
-    # Generating possible assignments for practices
-    for practice_id in schedule.remaining_practices:
-        for slot_id in Environment.PRACTICE_SLOT_IDS:
-            assignment = tuple(practice_id, slot_id)
-            if HardConstraints.check_constraints(schedule, assignment):
-                assignments.append()
+        # Generating possible assignments for games
+        for game_id in schedule.remaining_games:
+            for slot_id in Environment.GAME_SLOT_IDS:
+                assignment = tuple(game_id, slot_id)
+                if HardConstraints.check_constraints(schedule, assignment):
+                    assignments.append(assignment)
 
-    # Generating and returning new Schedule objects for each of these new assignments
-    return generate_schedules(schedule, assignments)
+        # Generating possible assignments for practices
+        for practice_id in schedule.remaining_practices:
+            for slot_id in Environment.PRACTICE_SLOT_IDS:
+                assignment = tuple(practice_id, slot_id)
+                if HardConstraints.check_constraints(schedule, assignment):
+                    assignments.append()
+
+        # Generating and returning new Schedule objects for each of these new assignments
+        return SearchModel.generate_schedules(schedule, assignments)
 
 
-def generate_schedules(schedule, assignments):
-    """
-    Input: List of assignments, where each assignment is represented by a 2-tuple containing an activity ID and a slot ID
-    Output: List of Schedule objects, where each corresponds to the addition of one of the new assignments given as input
-    """
-    schedules = [Schedule]
+    @staticmethod
+    def generate_schedules(schedule, assignments):
+        """
+        Input: List of assignments, where each assignment is represented by a 2-tuple containing an activity ID and a slot ID
+        Output: List of Schedule objects, where each corresponds to the addition of one of the new assignments given as input
+        """
+        schedules = [Schedule]
 
-    for assign in assignments:
-        schedule = schedule.get_copy()
-        activity_id, slot_id = assign
-        schedule.assign_activity(activity_id, slot_id)
-        schedules.append(schedule)
+        for assign in assignments:
+            schedule = schedule.get_copy()
+            activity_id, slot_id = assign
+            schedule.assign_activity(activity_id, slot_id)
+            schedules.append(schedule)
 
-    return schedules
+        return schedules
--- Search/Tree.py
+++ Search/Tree.py
@@ -5,24 +5,61 @@ leaf node will be a complete schedule. As such, every node in the tree will be a
 '''
 
 from ScheduleObjects.Schedule import Schedule
+from Search.SearchModel import SearchModel
+from Search.Environment import Environment
+
+
+class Node:
+    def __init__(self, parent, pr: Schedule, sol: bool):
+        self.parent = parent
+        self.pr = pr
+        self.sol = sol
+        self.children = []
+        self.opt = None
+        if self.parent != None:
+            self.compute_opt()
+
+    def check_sol(self):
+        # if sol becomes yes, call parent.check_sol()
+        pass
+
+    def compute_opt(self):
+        latest_id = self.pr.latest_assignment[0]
+
+        # If 
+        if latest_id in Environment.PARTASSIGN:
+            self.opt = float('-inf')
+        else:
+            self.opt = self.eval()
+
+    def eval(self):
+        # TODO: code functionality for computing eval and return the value
+        return 0
+
+    def add_child(self, parent, pr, sol):
+        self.children.append(Node(parent, pr, sol))
+
 
 class Tree:
+    current_stack = []
 
     # Empty constructor (TODO)
     def __init__(self) -> None:
-        self.root = self.Node(None, Schedule(), False)
+        self.root = Node(None, Schedule(), False)
+        Tree.current_stack.append(self.root)
 
-    class Node:
-        def __init__(self, parent, pr: Schedule, sol: bool):
-            self.parent = parent
-            self.pr = pr
-            self.sol = sol
-            self.children = []
 
-        def produce_children(self, schedules):
-            # div
-            pass
+    def erw(self, parent_node: Node):
+        for schedule in SearchModel.div(parent_node.pr):
+            parent_node.add_child(schedule, False)
 
-        def check_sol(self):
-            # if sol becomes yes, call parent.check_sol()
-            pass
+
+    def fleaf(self, parent_node: Node):
+        if not(len(parent_node.children) > 0):
+            raise(RuntimeError("fleaf attempting to choose child from parent with no children"))
+
+        # Sorting children by Opt values
+        parent_node.children.sort(key=lambda x: x.opt)
+
+        # Choosing leftmost child
+        return parent_node.children
