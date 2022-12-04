61388fb4f42c81e72060d3d881a4acfd5f4dc65f
jarint <56743248+jarint@users.noreply.github.com>
2022-12-03 17:17:20 -0700

    PracticeSlot update

--- ScheduleObjects/PracticeSlot.py
+++ ScheduleObjects/PracticeSlot.py
@@ -7,11 +7,12 @@ class PracticeSlot(ActivitySlot):
 
     ACTIVITY_TYPE = ActivityType.PRACTICE
 
-    def __init__(self, weekday: Weekday, time_str: str, start_time: str, duration: str, is_evening_slot: bool):
+    def __init__(self, weekday: Weekday, time_str: str, start_time: str, practicemax: int, practicemin: int, duration: str, is_evening_slot: bool):
         self.weekday = weekday
         self.time_str = time_str
         self.id = (self.ACTIVITY_TYPE, weekday, time_str)
-
+        self.practicemax= practicemax
+        self.practicemin = practicemin
         self.start_time = start_time
         self.end_time = start_time + duration
         self.duration = duration
